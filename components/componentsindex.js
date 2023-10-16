3cce25d84f8546329238f0598aee1b9aa1cc1803
rahul <96829896+rahul12st@users.noreply.github.com>
2023-10-16 22:29:35 +0530

    added Title template

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -4,5 +4,6 @@ import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+import Title from "./Title/Title";
 import Subscribe from "./Subscribe/Subscribe";
-export { Button, Footer, NavBar,HeroSection, Service,BigNFTSlider,Subscribe };
+export { Button, Footer, NavBar,HeroSection, Service,BigNFTSlider,Subscribe,Title };
