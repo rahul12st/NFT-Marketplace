12011a7d385ce9b0d3e1cc25578a2e0b3d140754
rahul <96829896+rahul12st@users.noreply.github.com>
2023-10-16 12:24:54 +0530

    created BigNFTslider

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -3,5 +3,5 @@ import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
-
-export { Button, Footer, NavBar,HeroSection, Service };
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+export { Button, Footer, NavBar,HeroSection, Service,BigNFTSlider };
