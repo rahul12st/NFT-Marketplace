d0074a1579c1d9e91e533a390db1f0174bf207ca
rahul <96829896+rahul12st@users.noreply.github.com>
2023-10-16 13:22:49 +0530

    added Subscribe section

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -4,4 +4,5 @@ import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
-export { Button, Footer, NavBar,HeroSection, Service,BigNFTSlider };
+import Subscribe from "./Subscribe/Subscribe";
+export { Button, Footer, NavBar,HeroSection, Service,BigNFTSlider,Subscribe };
