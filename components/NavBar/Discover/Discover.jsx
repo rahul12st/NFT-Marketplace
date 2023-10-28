9d9ed5cf6357af5ad76dcf3653bd368d20732738
rahul <96829896+rahul12st@users.noreply.github.com>
2023-10-28 13:23:24 +0530

    fixed some bugs

--- components/NavBar/Discover/Discover.jsx
+++ components/NavBar/Discover/Discover.jsx
@@ -36,9 +36,10 @@ const discover = [
 
   return (
     <div>
-      {discover.map((el, i)=> (
+      {discover.map((el, i)=> 
+        (
         <div key={i + 1} className={Style.discover}>
-          <Link href={{ pathname:'${el.link}'}}>{el.name}</Link>
+          <Link href={{ pathname:`${el.link}`}}>{el.name}</Link>
         </div>
       ))}
     </div>
