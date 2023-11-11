33f7969198af6aff2491480b48c225d5d96c25c2
rahul <96829896+rahul12st@users.noreply.github.com>
2023-11-11 16:24:37 +0530

    sidebar

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -2,7 +2,7 @@ import Discover from "./Discover/Discover";
 import HelpCenter from "./HelpCenter/HelpCenter";
 import Profile from "./Profile/Profile";
 import Notification from "./Notification/notification";
-import SideBar from "./SideBar/sideBar";
+import SideBar from "./SideBar/SideBar";
 
 
 export {Discover, HelpCenter, Profile, Notification, SideBar};
