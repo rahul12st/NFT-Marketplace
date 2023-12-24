48f147684761bcd80465a05db34b22ac6cfdc1c9
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2023-12-24 22:39:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
-    unoptimized: true, // Disable Next.js' default image optimization
+    unoptimized: false, // Disable Next.js' default image optimization
     domains: ["infura-ipfs.io",'rahul-nft-marketplace.infura-ipfs.io'] ,
     formats: ["image/webp"],
   },
